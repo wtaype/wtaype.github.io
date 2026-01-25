36a59279719d5fd7fa92affa1cf78e7eaabb7b37
wtaype <wilder.taype16@gmail.com>
2026-01-24 22:01:11 -0500

    Actualizacion Principal v9.10.10

--- smiles/wii.js
+++ smiles/wii.js
@@ -5,15 +5,15 @@ export let titulo = 'Portafolio de Wilder Taype'
 export let lanzamiento = 2024;
 export let autor = '@wilder.taype';
 export let link = 'https://wtaype.github.io/';
-export let version = 'v8';
+export let version = 'v9';
 
 /** Actualizar main luego esto, pero si es mucho, solo esto. (1)
-git tag v8 -m "Version v8" ; git push origin v8 
+git tag v9 -m "Version v9" ; git push origin v9 
 
 //  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
-git add . ; git commit -m "Actualizacion Principal v8.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v9.10.10" ; git push origin main
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
-git tag -d v8 ; git tag v8 -m "Version v8 actualizada" ; git push origin v8 --force
+git tag -d v9 ; git tag v9 -m "Version v9 actualizada" ; git push origin v9 --force
  ACTUALIZACION TAG[END]  */ 
 
