0ac26c2b570326f3af5e2c124f9edca1778f6115
wtaype <wilder.taype16@gmail.com>
2026-01-25 10:01:36 -0500

    Actualizacion Principal v14.10.11

--- smiles/wii.js
+++ smiles/wii.js
@@ -5,15 +5,15 @@ export let titulo = 'Portafolio de Wilder Taype'
 export let lanzamiento = 2024;
 export let autor = '@wilder.taype';
 export let link = 'https://wtaype.github.io/';
-export let version = 'v13';
+export let version = 'v14';
 
 /** Actualizar main luego esto, pero si es mucho, solo esto. (1)
-git tag v13 -m "Version v13" ; git push origin v13 
+git tag v14 -m "Version v14" ; git push origin v14 
 
 //  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
-git add . ; git commit -m "Actualizacion Principal v13.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v14.10.10" ; git push origin main
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
-git tag -d v13 ; git tag v13 -m "Version v13 actualizada" ; git push origin v13 --force
+git tag -d v14 ; git tag v14 -m "Version v14 actualizada" ; git push origin v14 --force
  ACTUALIZACION TAG[END]  */ 
 
