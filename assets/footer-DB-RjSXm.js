11b741c486c2d9fd1fd8a920344df6ab26bda6e7
wtaype <wtaype@users.noreply.github.com>
2026-03-15 05:34:49 +0000

    deploy: c01540d889d96d15ff44c9c7b1723d440cae9f52

---
+++ assets/footer-DB-RjSXm.js
@@ -0,0 +1,10 @@
+import{j as s}from"./vendor-gzd0YkcT.js";import{l as o,k as n,m as r,n as e,v as t}from"./main-DbUXgSKj.js";function l(){const a=new Date;return`
+  <footer class="foo wb txc psa">
+    <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${o}" target="_blank">${n}</a></span>
+    <span>${r} - <span class="wty">${a.getFullYear()}</span></span>
+    <span class="abw"> | ${e} ${t} | actualizado:
+    <span class="wtu">${a.toLocaleString()}</span></span>
+  </footer>
+  `}s("body").append(l());const p=`
+:root{--bgim:url("https://d35aaqx5ub95lt.cloudfront.net/images/star-pattern.svg")}.wicontainer{background: var(--bgim),linear-gradient(to bottom,var(--bg),var(--wb));}
+`;s("head").append(`<style>${p}</style> `);console.log("✅ Actualizado: "+t);export{l as footer};
