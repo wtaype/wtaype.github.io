cbae315135db9bd0bfcf8ef7198c6711dc2e82d9
wtaype <wtaype@users.noreply.github.com>
2026-03-15 06:05:20 +0000

    deploy: c3b12673b63be7b636bdd7d67a315193787230d8

---
+++ assets/footer-DLiF3Tl5.js
@@ -0,0 +1,10 @@
+import{j as s}from"./vendor-gzd0YkcT.js";import{l as o,k as t,m as n,n as r,v as e}from"./main-mTIHNNsD.js";function p(){const a=new Date;return`
+  <footer class="foo wb txc psa">
+    <span>Creado con <i class="fas fa-heart"></i> by <a class="ftx lkme" href="${o}" target="_blank">${t}</a></span>
+    <span>${n} - <span class="wty">${a.getFullYear()}</span></span>
+    <span class="abw"> | ${r} ${e} | actualizado:
+    <span class="wtu">${a.toLocaleString()}</span></span>
+  </footer>
+  `}s("body").append(p());const l=`
+:root{--bgim:url("/wpuntos.svg")}.wicontainer{background: var(--bgim),linear-gradient(to bottom,var(--bg),var(--wb));}
+`;s("head").append(`<style>${l}</style> `);export{p as footer};
